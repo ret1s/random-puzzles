be18d73aa75d1126994259cb7b6dedbcdbe9f3a4
geohanz <ngmhah@gmail.com>
2024-08-30 23:58:53 +0700

    update

--- codeforces/969_div2/A.java
+++ codeforces/969_div2/A.java
@@ -14,12 +14,14 @@ public class Doras_Set
             int l = scanner.nextInt(), r = scanner.nextInt();
             if ((r - l + 1) == 3 && l % 2 == 1) {
                 System.out.println(1);
-            } else if (l%2 == 1 && r%2 == 1) {
-                System.out.println((r - l)/4);
-            } else if (l%2 == 0 && r%2 == 0) {
-                System.out.println((r - l)/4);
             } else {
-                System.out.println((r - l + 1)/4);
+                if (l % 2 == 0) l++;
+                int m = r - l + 1;
+                if (m % 4 == 3) {
+                    System.out.println(m / 4 + 1);
+                } else {
+                    System.out.println(m / 4);
+                }
             }
         }
         scanner.close();
