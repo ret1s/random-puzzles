23d7f15ccbf18a9a7fc186728792a9456008e944
geohanz <ngmhah@gmail.com>
2024-09-04 16:51:04 +0700

    add: registration-login-demo user registration feature

--- spring-practice/registration-login-demo/src/main/java/geohanz/practice/registration_login_demo/controller/AuthController.java
+++ spring-practice/registration-login-demo/src/main/java/geohanz/practice/registration_login_demo/controller/AuthController.java
@@ -1,6 +1,9 @@
 package geohanz.practice.registration_login_demo.controller;
 
+import jakarta.validation.Valid;
+import geohanz.practice.registration_login_demo.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +12,13 @@ public class AuthController {
     @GetMapping("/index")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
     }
 
 }
