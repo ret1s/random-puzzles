c193b6058899e7c0bdc7b08577f26109e06f0cf7
ret1s <ngmhah@gmail.com>
2024-09-07 14:13:39 +0700

    update: registration-login-demo handle registration feature

--- spring-practice/registration-login-demo/src/main/java/geohanz/practice/registration_login_demo/controller/AuthController.java
+++ spring-practice/registration-login-demo/src/main/java/geohanz/practice/registration_login_demo/controller/AuthController.java
@@ -1,13 +1,24 @@
 package geohanz.practice.registration_login_demo.controller;
 
+import geohanz.practice.registration_login_demo.entity.User;
+import geohanz.practice.registration_login_demo.service.UserService;
 import jakarta.validation.Valid;
 import geohanz.practice.registration_login_demo.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+
+    private UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/index")
     public String home() {
@@ -21,5 +32,23 @@ public class AuthController {
         return "register";
     }
 
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
+                               BindingResult result,
+                               Model model) {
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+            result.rejectValue("email", null, "There is already an account registered with the same email.");
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
+    }
 }
 
