73139bdb8029c896f1c0181b70c8138f064a1098
ret1s <ngmhah@gmail.com>
2024-09-09 08:33:43 +0700

    update: leetcode daily challenge

---
+++ leetcode/2326.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int size = 0;
+        ListNode T = head;
+        while (T != null) {
+            size++;
+            T = T.next;
+        }
+
+        int[][] res = new int[m][];
+        for (int i = 0; i < m; i++) {
+            res[i] = new int[n];
+            Arrays.fill(res[i], -1);
+        }
+
+        int TR = 0, BR = m - 1, LC = 0, RC = n - 1;
+        while (head != null) {
+            for (int i = LC; i <= RC && head != null; i++) {
+                res[TR][i] = head.val;
+                head = head.next;
+            }
+            TR++;
+
+            for (int i = TR; i <= BR && head != null; i++) {
+                res[i][RC] = head.val;
+                head = head.next;
+            }
+            RC--;
+
+            for (int i = RC; i >= LC && head != null; i--) {
+                res[BR][i] = head.val;
+                head = head.next;
+            }
+            BR--;
+
+            for (int i = BR; i >= TR && head != null; i--) {
+                res[i][LC] = head.val;
+                head = head.next;
+            }
+            LC++;
+        }
+
+        return res;
+    }
+}
