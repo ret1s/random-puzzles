8fc0acf5098c61e5b06b075902f5f5c089ddc847
geohanz <ngmhah@gmail.com>
2024-09-25 18:18:02 +0700

    update: leetcode-daily-challenge

---
+++ leetcode/2416.java
@@ -0,0 +1,31 @@
+class Solution {
+    class Trie {
+        Trie[] ch = new Trie[26];
+        int visited = 0;
+    }
+
+    public int[] sumPrefixScores(String[] words) {
+        Trie trie = new Trie();
+        int[] ans = new int[words.length];
+        int k = 0;
+        for (String x : words) {
+            Trie t = trie;
+            for (int i = 0; i < x.length(); i++) {
+                int c = x.charAt(i) - 'a';
+                if (t.ch[c] == null) t.ch[c] = new Trie();
+                t.ch[c].visited++;
+                t = t.ch[c];
+            }
+        }    
+        for (String x : words) {
+            Trie t = trie; int curr = 0;
+            for (int i = 0; i < x.length(); i++) {
+                int c = x.charAt(i) - 'a';
+                curr += t.ch[c].visited;
+                t = t.ch[c];
+            }
+            ans[k++] = curr;
+        }
+        return ans;
+    }
+}
