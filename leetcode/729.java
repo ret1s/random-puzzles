a49355431ce13660f36c9c9e5ce6e35dd3aea4bf
ret1s <ngmhah@gmail.com>
2024-09-26 08:58:29 +0700

    update: leetcode daily challenge

---
+++ leetcode/729.java
@@ -0,0 +1,23 @@
+class MyCalendar {
+    private ArrayList<int[]> bookings;
+
+    public MyCalendar() {
+        bookings = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+        // Loop through all previously booked events
+        for (int[] event : bookings) {
+            int existingStart = event[0];
+            int existingEnd = event[1];
+            
+            // Check for overlap
+            if (start < existingEnd && end > existingStart) {
+                return false;
+            }
+        }
+        // No overlap, add the new event
+        bookings.add(new int[] {start, end});
+        return true;
+    }
+}
