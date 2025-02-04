faa2eadae10920a29c7a47cbfc42b7ba656bd0b4
Hanh Ngo-Minh <ngmhah@gmail.com>
2025-02-04 09:04:04 +0700

    fix

--- leetcode/3105.java
+++ leetcode/3105.java
@@ -3,10 +3,10 @@ class Solution {
         if (nums.length == 0) return 0;
         int maxLen = 1, inc = 1, dec = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > nums[i-1]) { inc++; dec = 1; }      // ↗️ Case
-            else if (nums[i] < nums[i-1]) { dec++; inc = 1; } // ↘️ Case
-            else { inc = 1; dec = 1; }                        // \U0001f7e1 Case
-            maxLen = Math.max(maxLen, Math.max(inc, dec));    // Update global max
+            if (nums[i] > nums[i-1]) { inc++; dec = 1; }      
+            else if (nums[i] < nums[i-1]) { dec++; inc = 1; } 
+            else { inc = 1; dec = 1; }                        
+            maxLen = Math.max(maxLen, Math.max(inc, dec));    
         }
         return maxLen;
     }
