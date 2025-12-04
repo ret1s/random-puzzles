8892ef8d793b6cec81524c7a14b4f9567b6af211
Hanh Ngo-Minh <ngmhah@gmail.com>
2025-12-04 15:22:45 +0700

    [advent-of-code] Add day 4 - part 1 solution

---
+++ advent-of-code-2025/day-4/p1.py
@@ -0,0 +1,80 @@
+"""
+--- Day 4: Printing Department ---
+You ride the escalator down to the printing department. They're clearly getting ready for Christmas; they have lots of large rolls of paper everywhere, and there's even a massive printer in the corner (to handle the really big print jobs).
+
+Decorating here will be easy: they can make their own decorations. What you really need is a way to get further into the North Pole base while the elevators are offline.
+
+"Actually, maybe we can help with that," one of the Elves replies when you ask for help. "We're pretty sure there's a cafeteria on the other side of the back wall. If we could break through the wall, you'd be able to keep moving. It's too bad all of our forklifts are so busy moving those big rolls of paper around."
+
+If you can optimize the work the forklifts are doing, maybe they would have time to spare to break through the wall.
+
+The rolls of paper (@) are arranged on a large grid; the Elves even have a helpful diagram (your puzzle input) indicating where everything is located.
+
+For example:
+
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+The forklifts can only access a roll of paper if there are fewer than four rolls of paper in the eight adjacent positions. If you can figure out which rolls of paper the forklifts can access, they'll spend less time looking and more time breaking down the wall to the cafeteria.
+
+In this example, there are 13 rolls of paper that can be accessed by a forklift (marked with x):
+
+..xx.xx@x.
+x@@.@.@.@@
+@@@@@.x.@@
+@.@@@@..@.
+x@.@@@@.@x
+.@@@@@@@.@
+.@.@.@.@@@
+x.@@@.@@@@
+.@@@@@@@@.
+x.x.@@@.x.
+Consider your complete diagram of the paper roll locations. How many rolls of paper can be accessed by a forklift?
+"""
+
+import os
+
+
+def solution():
+    input_path = os.path.join(os.path.dirname(__file__), "input.txt")
+    with open(input_path) as f:
+        grid = f.read().strip().split("\n")
+
+    if not grid or grid == [""]:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    directions = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),           (0, 1),
+        (1, -1),  (1, 0),  (1, 1),
+    ]
+
+    accessible = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] != "@":
+                continue
+
+            neighbors = 0
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < len(grid[nr]) and grid[nr][nc] == "@":
+                    neighbors += 1
+
+            if neighbors < 4:
+                accessible += 1
+
+    return accessible
+
+
+if __name__ == "__main__":
+    print(solution())
